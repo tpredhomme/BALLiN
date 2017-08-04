e2fda768382666e2de749be0458d3d32ef03fffa
kevin-sociaddict <kevin.fraissine@sociaddict.com>
2017-08-04 19:25:30 +0200

    Deploy config assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,12 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # - To access assets which are precompiled 
+  config.assets.digest = true
+
+  # - allowing caching assets
+  config.cache_classes = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
