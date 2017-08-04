ee077f5f76a067d58165d9cc326f3a763f3079f5
kevin-sociaddict <kevin.fraissine@sociaddict.com>
2017-08-04 19:17:45 +0200

    Groupe -> group

--- db/migrate/20170729075429_add_infos_to_teams.rb
+++ db/migrate/20170729075429_add_infos_to_teams.rb
@@ -3,6 +3,7 @@ class AddInfosToTeams < ActiveRecord::Migration[5.0]
     add_reference :teams, :category, foreign_key: true
     add_reference :teams, :level, foreign_key: true
     add_reference :teams, :division, foreign_key: true
-    add_reference :teams, :groupe, foreign_key: true
+    add_reference :teams, :group, foreign_key: true
   end
 end
+
--- db/migrate/20170729075957_rename_groupe_typo.rb
+++
@@ -1,5 +0,0 @@
-class RenameGroupeTypo < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :teams, :groupe_id, :group_id
-  end
-end
