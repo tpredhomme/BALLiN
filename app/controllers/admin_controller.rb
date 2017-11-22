bc279afd8b2101988d9355c6c7bf82464035dc64
kevin-sociaddict <kevin.fraissine@sociaddict.com>
2017-11-22 06:53:30 +0100

    Revision

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,3 @@
 #
 class AdminController < ApplicationController
-  before_action :allow_access
-
-  def allow_access
-    redirect_to root_path if !player || !player.try(:admin)
-  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,5 @@
 #
 class PlayersController < ApplicationController
-  # root_path - redirect admin to admin panel
-  before_action :redirect_admin, only: :show
   before_action :redirect_to_club_selection, only: :show
 
   def index
@@ -19,10 +17,6 @@ class PlayersController < ApplicationController
   end
 
   private
-
-  def redirect_admin
-    redirect_to admin_index_path if player.admin?
-  end
 
   def redirect_to_club_selection
     redirect_to selects_club_path unless player.club && player.team
